2604cb1750f4c098f905f7c2dd7eb490d8d53626
jakob.diebold <jakob.diebold@idealo.de>
2022-12-09 14:53:52 +0100

    refactor day09 to use same function

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -9,43 +9,28 @@ fn main() {
 }
 
 fn part_a() {
-    let binding = fs::read_to_string("inputs/day09/input.txt").unwrap();
-    let moves = binding.lines();
-    let mut visited_positions: HashSet<(i32, i32)> = HashSet::new();
-    visited_positions.insert((0, 0));
-    let directions = HashMap::from([("U", (0, 1)), ("D", (0, -1)), ("L", (-1, 0)), ("R", (1, 0))]);
-    let mut h_pos = (0, 0);
-    let mut t_pos = (0, 0);
-    for m in moves {
-        let cmd = m.split_ascii_whitespace().collect::<Vec<_>>();
-        let direction = directions.get(cmd[0]).unwrap();
-        let no_of_steps: i32 = cmd[1].parse().unwrap();
-        for _step in 0..no_of_steps {
-            h_pos = (h_pos.0 + direction.0, h_pos.1 + direction.1);
-            let diff: (i32, i32) = (h_pos.0 - t_pos.0, h_pos.1 - t_pos.1);
-            if diff.0.abs() > 1 || diff.1.abs() > 1 {
-                t_pos = (t_pos.0 + diff.0.signum(), t_pos.1 + diff.1.signum());
-            }
-            visited_positions.insert(t_pos);
-        }
-    }
-    println!("Part A: {}", visited_positions.len());
+    println!("Part A: {}", no_of_places_tail_visited(2));
 }
 
 fn part_b() {
+    println!("Part B: {}", no_of_places_tail_visited(10));
+}
+
+fn no_of_places_tail_visited(lenght_of_rope: usize) -> usize {
     let binding = fs::read_to_string("inputs/day09/input.txt").unwrap();
     let moves = binding.lines();
     let mut visited_positions: HashSet<(i32, i32)> = HashSet::new();
     visited_positions.insert((0, 0));
     let directions = HashMap::from([("U", (0, 1)), ("D", (0, -1)), ("L", (-1, 0)), ("R", (1, 0))]);
-    let mut rope = vec![(0, 0); 10];
+    let mut rope = vec![(0, 0); lenght_of_rope];
     for m in moves {
         let cmd = m.split_ascii_whitespace().collect::<Vec<_>>();
         let direction = directions.get(cmd[0]).unwrap();
         let no_of_steps: i32 = cmd[1].parse().unwrap();
         for _step in 0..no_of_steps {
-            rope[0] = (rope[0].0 + direction.0, rope[0].1 + direction.1);
+            rope[0] = (rope[0].0 + direction.0, rope[0].1 + direction.1); // move head
             for knot in 1..rope.len() {
+                // move knot
                 let diff: (i32, i32) = (
                     rope[knot - 1].0 - rope[knot].0,
                     rope[knot - 1].1 - rope[knot].1,
@@ -57,8 +42,8 @@ fn part_b() {
                     );
                 }
             }
-            visited_positions.insert(rope[9]);
+            visited_positions.insert(*rope.last().unwrap());
         }
     }
-    println!("Part B: {}", visited_positions.len());
+    visited_positions.len()
 }
